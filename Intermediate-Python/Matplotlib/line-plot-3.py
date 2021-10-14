0417c0c43a279beb9fb08a5a6ae103053c19017b
Diki Hamdani <36911342+Dikihmd01@users.noreply.github.com>
2021-10-14 23:14:23 +0700

    Update line-plot-3.py

--- Intermediate-Python/Matplotlib/line-plot-3.py
+++ Intermediate-Python/Matplotlib/line-plot-3.py
@@ -1,12 +1,9 @@
-# Print the last item from year and pop
-print(year[-1])
-print(pop[-1])
+# Print the last item of gdp_cap and life_exp
+print(gdp_cap[-1])
+print(life_exp[-1])
 
-# Import matplotlib.pyplot as plt
-import matplotlib.pyplot as plt
+# Make a line plot, gdp_cap on the x-axis, life_exp on the y-axis
+plt.plot(gdp_cap, life_exp)
 
-# Make a line plot: year on the x-axis, pop on the y-axis
-plt.plot(year, pop)
-
-# Display the plot with plt.show()
+# Display the plot
 plt.show()
